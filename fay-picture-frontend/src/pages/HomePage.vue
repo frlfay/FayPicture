902a97336f33afed6c5492da34309093df9c37f5
FLFfang <flffang28@icloud.com>
2025-03-13 23:52:32 +0800

    【项目初始化】- 前端

---
+++ fay-picture-frontend/src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = "项目初始化-前端";
+</script>
+
+<style scoped>
+#homePage {
+
+}
+</style>
